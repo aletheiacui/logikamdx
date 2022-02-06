957ef1778bd2c7a983933d35c89864ad68de623a
Aletheia Cui <aletheia@Aletheias-MacBook-Pro.local>
2022-02-06 20:36:31 +0100

    Reorganized directory.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='logikamdx',
     version='0.1',
     py_modules=['logikamdx'],
     install_requires = ['markdown>=3.0'],
+    packages=find_packages(include=['logikamdx', 'logikamdx.*']),
     entry_points={
-        'markdown.extensions': ['logikamdx = logika_mdx:LogikaExtension']
+        'markdown.extensions': ['logikamdx = logikamdx.logika_mdx:LogikaExtension']
     }
 )
