5dd27370820922d6de6730c4d92a9dada0613e0b
Aletheia Cui <aletheia@Aletheias-MacBook-Pro.local>
2022-02-06 20:09:38 +0100

    Added entry_points to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     version='0.1',
     py_modules=['logikamdx'],
     install_requires = ['markdown>=3.0'],
+    entry_points={
+        'markdown.extensions': ['logikamdx = logika_mdx:LogikaExtension']
+    }
 )
