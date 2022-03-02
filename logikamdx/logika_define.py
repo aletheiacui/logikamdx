50738528cb10e59273078802728254a430d596be
Aletheia Cui <aletheia@Aletheias-MacBook-Pro.local>
2022-03-02 08:53:51 +0100

    Added definition style options.

--- logikamdx/logika_define.py
+++ logikamdx/logika_define.py
@@ -20,16 +20,21 @@ class LogikaDefinePattern(InlineProcessor):
         else:
             wordform = re.sub(r'<.+?>', '', m.group(2))
             wordform = re.sub(r'\-{2}|\+{2}|\*|\_', '', wordform)
+        
+        if m.group(6):
+            define_style = m.group(6)
+            el.set("class", f"define-word define-{define_style}")
+        else:
+            el.set("class", f"define-word define-hover")
             
         el.set("name", wordform)
-        el.set("class", "define-wordform")
         el.text = m.group(2)
         return el, m.start(0), m.end(0)
 
 class LogikaDefineExtension(Extension):
     def extendMarkdown(self, md):
         # Add new patterns
-        DEFINE_PATTERN = r'(\{(?!\:)(.+?)\})(\((.+)\))?'
+        DEFINE_PATTERN = r'(\{(?!\:)(.+?)\})(\((.+?)\))?(\((.+?)\))?'
         md.inlinePatterns.register(LogikaDefinePattern(DEFINE_PATTERN, md), 'logika_define', 170)
 
 
